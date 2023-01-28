1aa0dc048ee635e91570ee8bcf1ecb0b3c2b943a
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-01-27 21:36:41 -0300

    adding cats, dogs etc options to show different gifs by clicking

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const img = document.querySelector('img');
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=cat', {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url
-  });
+function fetchGif(className, searchTerm) {
+  const img = document.querySelector(className);
+  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(response => {
+      img.src = response.data.images.original.url;
+    });
+}
+//cats
+fetchGif('.cats', 'cat');
+//dogs
+fetchGif('.dogs', 'dogs');
+//men
+fetchGif('.men', 'men');
+//women
+fetchGif('.women', 'women')
+//babies
+fetchGif('.babies', 'babies');
+//boys
+fetchGif('.boys', 'boys');
+//girls
+fetchGif('.girls', 'girls');
