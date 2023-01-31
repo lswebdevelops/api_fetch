716e695b109c3515d40d44ddc4ab57cc2a1e9161
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-01-30 22:58:20 -0300

    adding a button so the user can search for an object

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-function fetchGif(className, searchTerm) {
-  const img = document.querySelector(className);
-  fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`, {mode: 'cors'})
+const container = document.querySelector('.container');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const img = document.createElement('img');
+container.appendChild(img);
+img.classList.add('container');
+
+
+button.addEventListener('click', () => {
+  const searchTerm = input.value;
+  fetchGif(searchTerm);
+  }); 
+
+
+
+
+function fetchGif(searchTerm) {
+   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`,
+   {mode: 'cors'})
     .then(response => response.json())
     .then(response => {
       img.src = response.data.images.original.url;
-    });
+    })
+  .catch((error) => {
+    console.log("error: ", error);
+  });
 }
-//cats
-fetchGif('.cats', 'cat');
-//dogs
-fetchGif('.dogs', 'dogs');
-//men
-fetchGif('.men', 'men');
-//women
-fetchGif('.women', 'women')
-//babies
-fetchGif('.babies', 'babies');
-//boys
-fetchGif('.boys', 'boys');
-//girls
-fetchGif('.girls', 'girls');
+
+
