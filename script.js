119c1f333ff6c279a6a58440370df7d9b468b514
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-02-01 19:42:27 -0300

    adding async and await instead of then. adding try and catch to handle errors

--- script.js
+++ script.js
@@ -14,16 +14,19 @@ button.addEventListener('click', () => {
 
 
 
-function fetchGif(searchTerm) {
-   fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`,
+async function fetchGif(searchTerm) {
+ 
+ try {
+   const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=XjW9YGEYlR8XavZ8Su1WJJI6UOgqp33P&s=${searchTerm}`,
    {mode: 'cors'})
     .then(response => response.json())
     .then(response => {
       img.src = response.data.images.original.url;
     })
-  .catch((error) => {
+  }
+  catch(error){
     console.log("error: ", error);
-  });
+  };
 }
 
 
